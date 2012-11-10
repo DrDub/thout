39d311fb4d14b3201e66526dc4e6e49aeeadc40b
Pablo Duboue <pablo.duboue@gmail.com>
2012-11-10 16:06:20 -0500

    Static serving of client files.

---
+++ node/server.js
@@ -0,0 +1,15 @@
+//var WebSocketServer = require('websocket').server;
+var http = require('http');
+var static = require('node-static');
+
+var client_files = new(static.Server)('../client');
+
+var server = http.createServer(function(request, response) {
+    request.addListener('end', function () {
+        // Serve files
+        client_files.serve(request, response);
+    });
+});
+
+server.listen(8080);
+    
