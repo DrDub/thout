d1acdf43f5280c96cca9ec9b45f36574166c7908
Pablo Duboue <pablo.duboue@gmail.com>
2012-11-26 05:02:46 -0500

    cache rebalancing

--- node/cache.js
+++ node/cache.js
@@ -10,7 +10,7 @@ permit persons to whom the Software is furnished to do so, subject to
 the following conditions:
 
 The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
+dsincluded in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
@@ -21,12 +21,22 @@ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  
 */
 
-function Cache(){
-    this.connection_to_hash = {}; // connection to object hash -> notified
-    this.hash_to_connection = {}; // hash to list of connection, sort by notified
+function Cache(maxmem /* ignored */, replication_level, default_number_of_slots){
+    this.hashes = []; // a list of all hashes
+    this.connections = []; // a list of all hashes
+
+    this.connection_to_hash = {}; // connection to list of hash
+    this.connection_to_hash_time = {}; // connection to object hash -> notified
     this.connection_meta = {}; // connection to object
-       // known keys: 'number_of_slots'
-    this.DEFAULT_NUMBER_OF_SLOTS = 10; //TODO move to a config
+       // known keys: 
+       //  'number_of_slots'
+       //  'last_validated'
+       //  'number_of_hashes'
+    this.hash_to_connection = {}; // hash to list of connection, sort by notified
+    
+    this.DEFAULT_NUMBER_OF_SLOTS = default_number_of_slots;
+    this.REPLICATION_LEVEL = replication_level;
+    this.docfetcher = null;
 }
 
 // return the list of connection_ids that might have that hash,
@@ -44,27 +54,48 @@ function invalidate(connection_id, hash) {
         return; // shouldn't happen, bail out
 
     if(hash !== undefined){
-        delete this.connection_to_hash[connection_id][hash];
-        var pos = this.hash_to_connection[hash].indexOf(connection_id);
+        var pos;
+        pos = this.connection_to_hash[connection_id].indexOf(hash);
         if(pos > 0)
-            this.hash_to_connection[hash].split(pos,1);
+            this.connection_to_hash[connection_id].splice(pos,1);
+        delete this.connection_to_hash_time[connection_id][hash];
+        this.connection_meta[connection_id].number_of_hashes--;
+        pos = this.hash_to_connection[hash].indexOf(connection_id);
+        if(pos > 0)
+            this.hash_to_connection[hash].splice(pos,1);
+        pos = this.connections.indexOf(connection_id);
+        if(pos > 0)
+            this.connections.splice(pos,1);
         return;
     }
     // drop all for a given connection_id
     delete this.connection_to_hash[connection_id];
+    delete this.connection_to_hash_time[connection_id];
     for(hash in this.hash_to_connection){
         var pos = this.hash_to_connection[hash].indexOf(connection_id);
         if(pos > 0)
             this.hash_to_connection[hash].split(pos,1);
     }
+    var pos = this.connections.indexOf(connection_id);
+    if(pos > 0)
+        this.connections.splice(pos,1);
 }
 
-// internal function signaling a new connection
+// internal function for signaling a new connection
 function new_connection(connection_id){
-    this.connection_to_hash[connection_id]={};
+    this.connection_to_hash[connection_id] = [];
+    this.connection_to_hash_time[connection_id] = {};
     this.connection_meta[connection_id] = { 
-        'number_of_slots' : this.DEFAULT_NUMBER_OF_SLOTS 
+        'number_of_slots' : this.DEFAULT_NUMBER_OF_SLOTS,
+        'last_validated' : new Date(),
+        'number_of_hashes' : 0
     };
+}    
+
+// internal function for signaling a new hash
+function new_hash(hash) {
+    this.hash_to_connection[hash] = {};
+    this.hashes.push(hash);
 }    
 
 // we now know that connection_id has a given hash
@@ -73,11 +104,21 @@ function validate(connection_id, hash) {
         this.new_connection(connection_id);
 
     if(!(hash in this.hash_to_connection))
-        this.hash_to_connection[hash] = {};
+        this.new_hash(hash);
 
-    this.connection_to_hash[connection_id][hash] = new Date();
+    var pos;
+    pos = this.connection_to_hash[connection_id].indexOf(hash);
+    if(pos > 0)
+        this.connection_to_hash[connection_id].splice(pos,1);
+    else
+        this.connection_meta[connection_id].number_of_hashes++;
+
+    this.connection_to_hash[connection_id].push(hash); // most recent hashes at the end
+    this.connection_to_hash_time[connection_id][hash] = new Date();
+    this.connection_meta[connection_id].last_validated = new Date();
+
     // move connection_id to head
-    var pos = this.hash_to_connection[hash].indexOf(connection_id);
+    pos = this.hash_to_connection[hash].indexOf(connection_id);
     if(pos > 0)
         this.hash_to_connection[hash].split(pos,1);
     this.hash_to_connection[hash].splice(0,0,connection_id);
@@ -92,17 +133,19 @@ function capacity(connection_id, number_of_slots) {
 
 // A random sample of hashes, maybe biased by popularity
 function list(max_number_of_docs) {
+    //TODO bias this by popularity
+
     var result = [];
     var other = [];
     // Fisher-Yates up to max_number_of_docs, this should be much more
     // efficient if max_number_of_docs <~much~smaller~< number of hashes
     for(var i=0; i<max_number_of_docs; i++){
-        if(i == this.hash_to_connection.length)
+        if(i == this.hashes.length)
             return result;
 
-        var j = i + parseInt(Math.random() * (this.hash_to_connection.length - i));
-        var temp = other[j] === undefined ? this.hash_to_connection[j] : other[j];
-        other[j] = other[i] === undefined ? this.hash_to_connection[i] : other[i];
+        var j = i + parseInt(Math.random() * (this.hashes.length - i));
+        var temp = other[j] === undefined ? this.hashes[j] : other[j];
+        other[j] = other[i] === undefined ? this.hashes[i] : other[i];
         result[i] = temp;
     }
     return result;
@@ -110,7 +153,55 @@ function list(max_number_of_docs) {
    
 // Balance cache and issues DOCFETCHER->fetch(hash, [connection_id]) as needed
 function heartbeat() {
-    //TODO
+    // the following code assumes it'll be called fairly often with
+    // huge tables of hashes and connections so it just checks a few
+    // connections and hashes
+
+    var checked = {};
+    var under_represented = [];
+    var now = new Date();
+    // find under-represented
+    for(var i=0; i<50; i++){
+        var hash = this.hashes[parseInt(Math.random() * this.hashes.length)];
+        if(hash in checked)
+            continue;
+        checked[hash] = 1;
+        if(this.hash_to_connection[hash].length < this.REPLICATION_LEVEL)
+            under_represented.push(hash);
+        else{
+            // take the times to compute a timed average
+            var expected = 0.0;
+            for(connection_id in this.hash_to_connection[hash]){
+                expected += 600000. / (now - this.connection_to_hash[connection_id][hash]);
+                if(expected > this.REPLICATION_LEVEL)
+                    // note that this will hold with only one document recent enough
+                    break;
+            }
+            if(expected < this.REPLICATION_LEVEL)
+                under_represented.push(hash);
+        }
+    }
+
+    // find connections with available slots
+    checked = {};
+    for(var i=0; i<50; i++){
+        var connection_id = this.[parseInt(Math.random() * this.connections.length)];
+        if(connections in checked)
+            continue;
+        var should_send = false;
+        if(this.connection_to_hash[connection_id].length <  this.connection_meta[connection_id].number_of_slots)
+            should_send = true;
+        else if(now - this.connection_meta[connection_id].last_validated > 3600000)
+            should_send = true;
+        if(should_send)
+            for(var j=0; j<under_represented.length; j++){
+                if(this.connection_to_hash_time[connection_id][under_presented] === undefined){
+                    this.docfetcher.fetch(hash, connection_id);
+                    under_represented.splice(j,1);
+                    break;
+                }
+            }
+    }
 }
 
 Cache.prototype.find_by_hash = find_by_hash;
